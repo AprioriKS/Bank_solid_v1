fc59c51123743b38bda0b08b0336734000991ad3
AprioriKS <sounddummies@gmail.com>
2022-02-20 12:25:51 +0200

    Main initialization add method

--- src/mate.academy/Main.java
+++ src/mate.academy/Main.java
@@ -19,20 +19,23 @@ public class Main {
     public static void main(String[] args) {
         Map<Type, CommissionHandler> commissionHandlerMap = new HashMap<>();
 
-        Storage.accounts.add(new Account("5788", new BigDecimal(5000), Type.GOLD));
-        Storage.accounts.add(new Account("3456", new BigDecimal(10000), Type.PLATINUM));
-        Storage.accounts.add(new Account("1233", new BigDecimal(15000), Type.REGULAR));
-        Storage.accounts.add(new Account("1245", new BigDecimal(5000), Type.GOLD));
-
-
-        commissionHandlerMap.put(Type.GOLD, new GoldCommissionHandler());
-        commissionHandlerMap.put(Type.PLATINUM, new PlatinumCommissionHandler());
-        commissionHandlerMap.put(Type.USUAL, new UsualCommissionHandler());
-        commissionHandlerMap.put(Type.REGULAR, new RegularCommissionHandler());
+        startInit(commissionHandlerMap);
 
         CommissionStrategy commissionStrategy = new CommissionStrategyImpl(commissionHandlerMap);
         BankService bankService = new BankServiceImpl(new AccountDaoImpl(), commissionStrategy);
 
         bankService.transfer("1233", "5788", BigDecimal.valueOf(1000));
+    }
+
+    private static void startInit(Map<Type, CommissionHandler> commissionHandlerMap) {
+        Storage.accounts.add(new Account("5788", new BigDecimal(5000), Type.GOLD));
+        Storage.accounts.add(new Account("3456", new BigDecimal(10000), Type.PLATINUM));
+        Storage.accounts.add(new Account("1233", new BigDecimal(15000), Type.REGULAR));
+        Storage.accounts.add(new Account("1245", new BigDecimal(5000), Type.GOLD));
+
+        commissionHandlerMap.put(Type.GOLD, new GoldCommissionHandler());
+        commissionHandlerMap.put(Type.PLATINUM, new PlatinumCommissionHandler());
+        commissionHandlerMap.put(Type.USUAL, new UsualCommissionHandler());
+        commissionHandlerMap.put(Type.REGULAR, new RegularCommissionHandler());
     }
 }
